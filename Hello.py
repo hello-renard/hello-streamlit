1b03476399076b5e64f0ef49d580c30895a6dda5
hello-renard <166446155+hello-renard@users.noreply.github.com>
2024-04-08 21:50:15 +0000

    first version

--- Hello.py
+++ Hello.py
@@ -1,51 +1,27 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
+from langchain_community.document_loaders import AsyncHtmlLoader
+from langchain_community.document_transformers import Html2TextTransformer
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_anthropic import ChatAnthropic
+from config import OPENAI_API_KEY
+
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY,model="gpt-4-turbo-preview",temperature=0.7)
 
 LOGGER = get_logger(__name__)
+st.write("# Welcome to hello again Push AI")
+website = st.text_input("Please enter your Website",help="Enter a website in the pattern of https://www.website.at")
 
+#Fetch website
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+result = st.button("Start")
+if result:
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+  loader = AsyncHtmlLoader(website)
+  docs = loader.load()
+  html2text = Html2TextTransformer()
+  docs_transformed = html2text.transform_documents(docs)
+  docs_transformed[0].page_content[0:10000]
+  
