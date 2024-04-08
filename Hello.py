ce8237c531be9c2f85bfb2172ab9e3d55e0db62f
hello-renard <166446155+hello-renard@users.noreply.github.com>
2024-04-08 21:58:33 +0000

    first release

--- Hello.py
+++ Hello.py
@@ -23,5 +23,40 @@ if result:
   docs = loader.load()
   html2text = Html2TextTransformer()
   docs_transformed = html2text.transform_documents(docs)
-  docs_transformed[0].page_content[0:10000]
+  chat_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Ein Nutzer stellt dir extrahierte Texte einer Webseite eines Unterhehmens zur Verfügung. Deine Aufgabe ist es folgende Informationen herauszufinden:"
+        +"#Name des Unternehmens:# (Wähle dabei den Namen den das Unternehmen in seiner Kommunikation verwendet)\n" 
+        +"#Liste angebotener Dienstleistungen und Produkte:# (Wähle dabei maximal 5 Überkategorien\n"
+        +"#Vorteile und USPS:# (Wähle dabei die relevantesten USPs. Maximal 4)\n"+
+        "#Tonalität und Stil:# (Kommuniziert das Unternehmen per DU mit Kunden, wie förmlich oder locker ist der Stil?"),
+        ("human", "{websiteText}")
+    ]
+)
+
+  messages = chat_template.format_messages(
+  websiteText=docs_transformed)
+
+
+  companyData = llm.invoke(messages).content
+  v_occasion = "Aktion oder Anlass: kein spezieller Anlass"
+  v_goal ="Ziel der Nachricht: Kunden zurück ins Geschäft holen\n"
+
+  chat_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Du bist ein Kommunikationsspezialist und schreibst 3 Vorschläge einer maßgeschneiderte Push Nachricht auf Basis der vom Nutzer bereitgestellten Unternehmensdetails. Beachte die Details um eine möglichst effektive, wahrheitsgemäße Nachricht zugeschnitten auf das Unternehmen zu verfassen. Jede Push Nachricht soll aus einem title mit maximal 70 Zeichen und einer description mit maximal 150 Zeichen bestehen. Vermeide Angebote, Vorteile oder Aktionen vorzuschlagen die nicht aus den angegebenen Inhalten klar und deutlich hervorgehen. \n\nAusgabeformat:\nBegründe jeden deiner Vorschläge mit einem reasoning mit maximal 150 zeichen. Die Ausabe muss in einem JSON Objekt 'pushmessages' mit den 3 Vorschlägen als Objekten. Jedes Objekt enthält eine ID (1-3) title, description und reasoning."),
+        ("human", "{companyDetails}" 
+        +  "{occasion}"
+        +  "{goal}"
+        )
+    ]
+  )
+
+  messages = chat_template.format_messages(
+  companyDetails=companyData,
+  occasion=v_occasion,
+  goal=v_goal)
+
+  proposedMessages = llm.invoke(messages)
+  st.markdown(proposedMessages.content)
   
